51e1c99d4f04cadb863df1b00619045708bbe334
Thomas Lau <liujiacheng@sj56.com.cn>
2025-08-12 16:06:33 +0800

    init projects

---
+++ Pomeranian/src/main/java/cn/com/sanga/pomeranian/PomeranianApplication.java
@@ -0,0 +1,13 @@
+package cn.com.sanga.pomeranian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomeranianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomeranianApplication.class, args);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-1
+Chihuahua 前端项目
+Pomeranian 后端项目
+Schnauzer 移动端项目
